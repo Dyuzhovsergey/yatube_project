29942353078eb9cd2cbec2270b366ab48eb6f892
Dyuzhov Sergey <dyuzhovsergey@gmail.com>
2024-03-10 12:45:38 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def index(request):    
+    return HttpResponse('Главная страница YaTube')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Это пост группы: "{slug}"')
